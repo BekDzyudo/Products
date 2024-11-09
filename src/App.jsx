fcb5836b4674eff71e6fc78e35277c6f0863f977
shohbek <jalilovshohbek9909@gmail.com>
2024-11-09 16:30:22 +0500

    fix

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <button className="btn btn-primary btn-sm">click</button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/fetures/productsSlice.js
@@ -0,0 +1,7 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+  allProducts: 0,
+  price: 0,
+};
